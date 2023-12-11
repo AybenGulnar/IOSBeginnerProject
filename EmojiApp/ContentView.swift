ffd5c95fadacb92fc8d28d2718dd9296a79f0a69
aybengulnar <ayben.glnar@gmail.com>
2023-12-11 20:20:28 +0300

    Initial Commit

---
+++ EmojiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiApp
+//
+//  Created by Ayben Gülnar  on 11.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Ayben Gülnar  on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
